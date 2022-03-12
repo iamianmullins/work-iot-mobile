5ac48c222a7175f5c5eeec073950c4475d907656
Ian Mullins <mullins.89@hotmail.com>
2022-03-12 12:52:09 +0000

    Initial commit - Basic project

---
+++ app/src/main/java/ie/wit/work_iot_mobile/Workout.kt
@@ -0,0 +1,11 @@
+package ie.wit.work_iot_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Workout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout)
+    }
+}
